ca3f6acfbba95bd6b60da0f3d28711a832fc9225
Caio Tenório <126365047+caiomatenorio@users.noreply.github.com>
2025-05-19 21:46:01 -0300

    Refactor message namespace

--- src/namespaces/message_namespace.py
+++ src/namespaces/message_namespace.py
@@ -12,21 +12,23 @@ from ..decorators.requires_socket_auth import requires_socket_auth
 from ..libs.socketio import socketio
 from ..services import session_service
 
+namespace = "/sockets/messages"
 
-@socketio.event(namespace="/sockets/messages")
+
+@socketio.event(namespace=namespace)
 @handle_socket_with_auth_connection
 def connect():
     pass
 
 
-@socketio.event(namespace="/sockets/messages")
+@socketio.event(namespace=namespace)
 @requires_socket_auth
 def message(data):
     tokens = session_service.get_new_tokens(in_json=True)
     send(tokens)
 
 
-@socketio.event(namespace="/sockets/messages")
+@socketio.event(namespace=namespace)
 @handle_socket_with_auth_disconnection
 def disconnect():
     pass
