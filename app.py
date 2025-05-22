dc0460c04d5acc2907af7484c636afbfd6a9e197
Caio Tenório <caiomatenorio@gmail.com>
2025-05-22 19:19:01 -0300

    Add Dockerfile and docker-compose.yml; refactor API blueprint and ping controller

--- app.py
+++ app.py
@@ -1,15 +1,6 @@
 from src.singleton.app import app
-from src.singleton.env import env
 from src.singleton.socketio import socketio
 
 if __name__ == "__main__":
-    import eventlet
-
-    match env.FLASK_ENV:
-        case "development":
-            import eventlet.wsgi  # development server
-        case "production":
-            import gunicorn  # production server
-
     socketio.run(app, debug=True, host="0.0.0.0", port=5000)
     print("Server started.")
--- src/api/controller/ping_controller.py
+++ src/api/controller/ping_controller.py
@@ -2,6 +2,11 @@ from src.api.blueprint.api_bp import api_bp
 from src.api.response_body.success_response_body import SuccessResponseBody
 
 
+@api_bp.get("")
+def index():
+    return SuccessResponseBody(200, "Welcome to the API!").to_response()
+
+
 @api_bp.get("/ping")
 def ping():
     return SuccessResponseBody(200, "Pong!").to_response()
